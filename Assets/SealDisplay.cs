2849c092fa9c16edcd9b4370d1eb0e8fa13bc902
Laverden <nlav05@gmail.com>
2020-11-03 14:29:11 +0100

    Scriptable seal tag and displayer

---
+++ Assets/SealDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SealDisplay : MonoBehaviour
+{
+    public SealTag sealTag;
+
+    public Text sealName;
+    public Image sealCategoryIcon;
+    public Image sealTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sealName.text = sealTag.sealName;
+        sealCategoryIcon.sprite = sealTag.sealCategory;
+        sealTemplate.color = sealTag.sealColor;
+    }
+
+}
---
+++ Assets/SealTag.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sealtag", menuName = "Seal")]
+public class SealTag : ScriptableObject
+{
+    public string sealName;
+
+    public Color sealColor;
+    public Sprite sealCategory;
+}
