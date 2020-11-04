b7d69012df98a17ec619a9331bb285b58ba63219
Laverden <nlav05@gmail.com>
2020-11-04 15:06:02 +0100

    Custom editor based on serialize class and fields

---
+++ Assets/Editor/AppDataObjectCustomEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class AssetHandler
+{
+    [OnOpenAsset()]
+    public static bool OpenEditor (int instaceId, int line)
+    {
+        AppDataObject obj = EditorUtility.InstanceIDToObject(instaceId) as AppDataObject;
+
+        if (obj != null)
+        {
+            AppDataObjectEditorWindow.Open(obj);
+            return true;
+        }
+
+        return false;
+    }
+}
+
+[CustomEditor(typeof(AppDataObject))]
+public class AppDataObjectCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Open Editor"))
+        {
+            AppDataObjectEditorWindow.Open((AppDataObject)target);
+        }
+    }
+}
---
+++ Assets/Editor/AppDataObjectEditorWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class AppDataObjectEditorWindow : ExtendedEditorWindow
+{
+    public static void Open(AppDataObject dataObject)
+    {
+        AppDataObjectEditorWindow window = GetWindow<AppDataObjectEditorWindow>("App Data Editor");
+        window.serializedObject = new SerializedObject(dataObject);
+    }
+
+    private void OnGUI()
+    {
+        List<string> properties = new List<string>()
+        {
+            "appInformation",
+            "develInformation"
+        };
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.BeginVertical("box", GUILayout.MaxWidth(150), GUILayout.ExpandHeight(true));
+
+        foreach (string pName in properties)
+        {
+            currentProperty = serializedObject.FindProperty(pName);
+            DrawSidebarBasedOnClass(currentProperty);
+            //DrawSidebar(currentProperty);
+        }
+        
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical("box", GUILayout.ExpandHeight(true));
+
+        if (selectedProperty != null)
+        {
+            DrawProperties(selectedProperty, true);
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Select and item from the list");
+        }
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+
+        Apply();
+    }
+}
