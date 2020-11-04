77291ece63ac49144567755ac6dd1a30fdd4bdaf
Laverden <nlav05@gmail.com>
2020-11-04 11:39:06 +0100

    Serialized class setup for custom editor tutorial

---
+++ Assets/_Scripts/AppDataObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AppDataObject", menuName = "AppData")]
+public class AppDataObject : ScriptableObject
+{
+    [SerializeField]
+    private string version;
+
+    public AppInfo appInformation;
+}
+
+[Serializable]
+public class AppInfo
+{
+    [SerializeField]
+    private string name;
+
+    [SerializeField]
+    private string companyName;
+
+    public string maintainer;
+    public string email;
+
+    public BuildInfo buildInfo;
+
+    [Serializable]
+    public class BuildInfo
+    {
+        [SerializeField]
+        private string bundleID;
+
+        [TextArea(3, 10)]
+        public string description;
+    }
+}
