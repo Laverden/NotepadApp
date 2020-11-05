dc66281f3c3cf244321523c633ce005de139a4ec
Laverden <nlav05@gmail.com>
2020-11-05 11:27:23 +0100

    Localization system

---
+++ Assets/Utilities/TextLocalizerUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLocalizerUI : MonoBehaviour
+{
+    Text textField;
+
+    public string key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<Text>();
+        string value = LocalizationSystem.GetLocalizedValue(key);
+        textField.text = value;
+    }
+}
