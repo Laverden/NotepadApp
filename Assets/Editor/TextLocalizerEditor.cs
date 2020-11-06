7b57d9e520ee6e733ee8c1ddbfb0bc0803b0777d
Laverden <nlav05@gmail.com>
2020-11-06 11:12:16 +0100

    Custom editor windows for localized system

---
+++ Assets/Editor/TextLocalizerEditor.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEditor;
+using UnityEngine;
+
+public class TextLocalizerEditWindow : EditorWindow
+{
+    public static void Open(string key)
+    {
+        TextLocalizerEditWindow window = ScriptableObject.CreateInstance<TextLocalizerEditWindow>();
+        window.titleContent = new GUIContent("Localizer Window");
+        window.ShowUtility();
+        window.key = key;
+    }
+
+    public string key;
+    public string value;
+
+    public void OnGUI()
+    {
+        key = EditorGUILayout.TextField("Key :", key);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Value:", GUILayout.MaxWidth(50));
+
+        EditorStyles.textArea.wordWrap = true;
+        value = EditorGUILayout.TextArea(value, EditorStyles.textArea, GUILayout.Height(100), GUILayout.Width(400));
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Add"))
+        {
+            if (LocalizationSystem.GetLocalizedValue(key) != string.Empty)
+            {
+                LocalizationSystem.Replace(key, value);
+            }
+            else
+            {
+                LocalizationSystem.Add(key, value);
+            }
+        }
+
+        minSize = new Vector2(460, 250);
+        maxSize = minSize;
+    }
+}
+
+public class TextLocalizerSearchWindow : EditorWindow
+{
+    public static void Open()
+    {
+        TextLocalizerSearchWindow window = ScriptableObject.CreateInstance<TextLocalizerSearchWindow>();
+        window.titleContent = new GUIContent("Localization Search");
+
+        Vector2 mouse = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+        Rect r = new Rect(mouse.x - 450, mouse.y + 10, 10, 10);
+        window.ShowAsDropDown(r, new Vector2(500, 300));
+    }
+
+    public string value;
+    public Vector2 scroll;
+    public Dictionary<string, string> dictionary;
+
+    private void OnEnable()
+    {
+        dictionary = LocalizationSystem.GetDictionaryForEditor();
+    }
+
+    public void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal("Box");
+        EditorGUILayout.LabelField("Search: ", EditorStyles.boldLabel);
+        value = EditorGUILayout.TextField(value);
+        EditorGUILayout.EndHorizontal();
+
+        GetSearchResults();
+    }
+
+    private void GetSearchResults()
+    {
+        if (value == null) { return; }
+
+        EditorGUILayout.BeginVertical();
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+
+        foreach (KeyValuePair<string, string> element in dictionary)
+        {
+            if (element.Key.ToLower().Contains(value.ToLower()) || element.Value.ToLower().Contains(value.ToLower()))
+            {
+                EditorGUILayout.BeginHorizontal("box");
+                Texture closeIcon = (Texture)Resources.Load("trash");
+
+                GUIContent content = new GUIContent(closeIcon, "Delete");
+
+                if (GUILayout.Button(content, GUILayout.MaxWidth(20)))
+                {
+                    string warningMsg = "This will remove the element from the localization, are you sure?";
+                    string confirmationMsg = "Do it";
+
+                    if (EditorUtility.DisplayDialog("Remove Key " + element.Key + "?", warningMsg, confirmationMsg))
+                    {
+                        LocalizationSystem.Remove(element.Key);
+                        AssetDatabase.Refresh();
+                        LocalizationSystem.Init();
+                        dictionary = LocalizationSystem.GetDictionaryForEditor();
+                    }
+                }
+
+                EditorGUILayout.TextField(element.Key);
+                EditorGUILayout.LabelField(element.Value);
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+}
